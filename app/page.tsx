f0aa05404f39540f15c21f4aee8f9298c19dd80e
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-05-11 15:57:30 +0530

    Added plausible

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { IPL } from "@/components/ipl";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PlausibleProvider from "next-plausible";
 
 const queryClient = new QueryClient();
 
 export default function Home() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <IPL />
-    </QueryClientProvider>
+    <PlausibleProvider domain={window.location.hostname}>
+      <QueryClientProvider client={queryClient}>
+        <IPL />
+      </QueryClientProvider>
+    </PlausibleProvider>
   );
 }
