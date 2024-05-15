81f6339a342cb6bc118724b90da5e9bc369ffafa
Manoj Vivek <p.manoj.vivek@gmail.com>
2024-05-15 09:42:56 +0530

    Added github link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { IPL } from "@/components/ipl";
+import { Icon } from "@iconify/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -7,6 +8,13 @@ const queryClient = new QueryClient();
 export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
+      <a
+        href="https://github.com/manojVivek/ipl-qualification-chances"
+        className="absolute top-5 right-5"
+        target="_blank"
+      >
+        <Icon icon="mdi:github" height={32} width={32} />
+      </a>
       <IPL />
     </QueryClientProvider>
   );
